a7bfc422fa142f18ec0f6192b124d399ec771b08
meghakhot137-hash <meghakhot137@gmail.com>
2026-01-12 17:09:23 +0530

    Add files via upload

---
+++ assigement4.js
@@ -0,0 +1,9 @@
+let a=5,b=10,c=15;
+if(a>=b&&a>=c){
+    console.log("the max number:"+a)
+
+}else if(b>=a&&b>=c){
+    console.log("the max number:"+b)
+}else{
+    console.log("the max number:"+c)
+}
